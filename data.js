d1b5b6aa12e623044ebd6c69c06a1dec7e720ad3
Praveen <praveenkumarchinthapalli721@gmail.com>
2023-09-25 20:53:56 +0530

    intial commit

---
+++ data.js
@@ -0,0 +1,64 @@
+let speedTypingTestEl = document.getElementById("speedTypingTest");
+let timerEl = document.getElementById("timer");
+let quoteDisplayEl = document.getElementById("quoteDisplay");
+let resultEl = document.getElementById("result");
+let quoteInputEl = document.getElementById("quoteInput");
+let submitBtnEl = document.getElementById("submitBtn");
+let resetBtnEl = document.getElementById("resetBtn");
+
+
+let countdown = 0;
+let intervalId;
+function timer(){
+    countdown = countdown + 1;
+    timerEl.textContent = countdown + " ";
+    timerEl.style.color = "red";
+}
+
+function clear() {
+    countdown = 0
+    timerEl.textContent = countdown + " ";
+    clearInterval(intervalId);
+}
+
+function randomQuote() {
+    let options = {
+        method: "GET"
+    };
+    let url = "https://apis.ccbp.in/random-quote";
+    fetch(url, options)
+        .then(function(response) {
+            timer();
+            intervalId = setInterval(timer, 1000);
+            return response.json();
+        })
+        .then(function(jsondata) {
+
+            let randomQuote = jsondata.content;
+            quoteDisplayEl.textContent = randomQuote;
+        });
+}
+randomQuote();
+
+
+resetBtnEl.onclick = function() {
+    
+    quoteInputEl.value = "";
+    resultEl.textContent = "";
+    clear();
+    randomQuote();
+};
+
+submitBtnEl.onclick = function() {
+    let textEl = quoteInputEl.value;
+    let quote = quoteDisplayEl.textContent;
+    if (quote === textEl) {
+        resultEl.textContent = "You typed in " + countdown + " seconds";
+        resultEl.style.color = "blue";
+        clearInterval(intervalId);
+    } else {
+        resultEl.textContent = "You typed incorrect sentence";
+        resultEl.style.color = "red";
+    }
+};
+
